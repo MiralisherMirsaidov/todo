46f9fa0e61bf1fdacd5ecdf78fa1e9fe7c0bdb4f
MiralisherMirsaidov <mirclash0109@gmail.com>
2022-10-30 23:54:30 +0500

    exam todo

---
+++ data.js
@@ -0,0 +1,195 @@
+let data = [
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Floyd Miles",
+        group: "Yahoo",
+        phoneNumber: "(205) 555-0100",
+        email: "floyd@yahoo.com",
+        groupName: "Frontend 010",
+        status: "false"
+    },
+    {
+        name: "Ronald Richards",
+        group: "Adobe",
+        phoneNumber: "(302) 555-0107",
+        email: "ronald@adobe.com",
+        groupName: "Frontend 001",
+        status: "false"
+    },
+    {
+        name: "Marvin McKinney",
+        group: "Tesla",
+        phoneNumber: "(252) 555-0126",
+        email: "marvin@tesla.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jerome Bell",
+        group: "Google",
+        phoneNumber: "(629) 555-0129",
+        email: "jerome@google.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Kathryn Murphy",
+        group: "Microsoft",
+        phoneNumber: "(406) 555-0120",
+        email: "kathryn@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jacob Jones",
+        group: "Yahoo",
+        phoneNumber: "(208) 555-0112",
+        email: "jacob@yahoo.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Kristin Watson",
+        group: "Facebook",
+        phoneNumber: "(704) 555-0127",
+        email: "kristin@facebook.com",
+        groupName: "Frontend 010",
+        status: "false"
+    },
+    {
+        name: "Stive Jobs",
+        group: "Apple",
+        phoneNumber: "(225) 666-0228",
+        email: "jobs@apple.com",
+        groupName: "Frontend 018",
+        status: "false"
+    },
+    {
+        name: "Bill Geyts",
+        group: "Microsoft",
+        phoneNumber: "(225) 575-5518",
+        email: "geyts@microsoft.com",
+        groupName: "Frontend 003",
+        status: "true"
+    },
+    {
+        name: "Alfonso de Paul",
+        group: "Yandex",
+        phoneNumber: "(561) 895-9865",
+        email: "paul@yandex.ru",
+        groupName: "Frontend 005",
+        status: "true"
+    },
+    {
+        name: "Josh Dark",
+        group: "Yahoo",
+        phoneNumber: "(547) 785-1236",
+        email: "dark@yahoo.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Soler",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "soler@microsoft.com",
+        groupName: "Frontend 006",
+        status: "false"
+    },
+    {
+        name: "Balde",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "balde@microsoft.com",
+        groupName: "Frontend 010",
+        status: "false"
+    },
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+    {
+        name: "Jane Cooper",
+        group: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        groupName: "Frontend 010",
+        status: "true"
+    },
+
+]
---
+++ main.js
@@ -0,0 +1,150 @@
+let users = $("#users")
+
+
+// userlarni chizib olish start
+
+makeCard(data)
+function makeCard(param, index) {
+    users.html("")
+    param.map((arr) => {
+        let people = `<tr>
+        <td>${arr.name}</td>
+        <td>${arr.group}</td> 
+        <td>${arr.phoneNumber}</td> 
+        <td>${arr.email}</td> 
+        <td>${arr.groupName}</td> 
+        <td><span class="badge ${arr.status && `badgeSucsess` || `badgeDanger`}">
+        ${arr.status && `Active` || `Inactive`}
+        </span></td> 
+        </tr>
+        `
+        users.append(people)
+    })
+}
+
+// userlarni chizib olish end
+
+
+
+// search start
+
+function search(text) {
+    let result = data.filter((arr) => {
+        return arr.name.toLowerCase().includes(text.value.toLowerCase())
+    })
+    makeCard(result)
+}
+
+// search end
+
+
+
+// nomerini chiqarish start
+
+function mintable() {
+    numberData = []
+    if (numberData.length < data.length) {
+        for (let i = 0; i < 7; i++) {
+            numberData.push(data[i])
+        }
+        makeCard(numberData)
+    }
+}
+
+mintable()
+
+function one() {
+    numberData = []
+    if (numberData.length < data.length) {
+        for (let i = 0; i < 7; i++) {
+            numberData.push(data[i])
+        }
+        makeCard(numberData)
+
+    }
+}
+
+function two() {
+    numberData = []
+    if (numberData.length < data.length) {
+        for (let i = 8; i < 15; i++) {
+            numberData.push(data[i])
+        }
+        makeCard(numberData)
+        
+    }
+}
+
+function three() {
+    numberData = []
+    if (numberData.length < data.length) {
+        for (let i = 16; i < 23; i++) {
+            numberData.push(data[i])
+        }
+        makeCard(numberData)
+    }
+}
+
+function four() {
+    numberData = []
+    if (numberData.length < data.length) {
+        for (let i = 24; i < 30; i++) {
+            numberData.push(data[i])
+        }
+        makeCard(numberData)
+    }
+}
+
+// nomerini chiqarish end
+
+
+
+// modalga chizish start
+
+function addUsers() {
+    users.html("")
+
+    let ismi = $(".ism").val()
+    let sana = $(".sana").val()
+    let guruh = $(".guruh").val()
+    let tolov = $(".tolov").val()
+    let emaili = $(".email").val()
+    let erkak = $(".erkak").val()
+    let ayol = $(".ayol").val()
+    let telefon = $(".telefon").val()
+
+    console.log(ismi);
+    console.log(guruh);
+
+    data.push({
+        name: ismi,
+        data: sana,
+        group: guruh,
+        pay: tolov,
+        email: emaili,
+        men: erkak,
+        woman: ayol,
+        phone:telefon
+    })
+    drawNewUsers(data)
+}
+
+function drawNewUsers(data) {
+    users.html("")
+    data.map((arr) => {
+        let newUsers = `<tr>
+        <td>${arr.name}</td>
+        <td>${arr.data}</td> 
+        <td>${arr.group}</td> 
+        <td>${arr.pay}</td> 
+        <td>${arr.email}</td> 
+        <td>${arr.men}</td> 
+        <td>${arr.woman}</td> 
+        <td>${arr.phone}</td> 
+        </tr>
+        `
+        users.append(newUsers)
+    })
+}
+
+// modalga chizish end
